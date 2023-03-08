48e728f2b927e246a99fe5085bdd705932d58243
Kunobi Ozuma <student.150267@worcesterschools.net>
2023-03-07 20:57:23 -0500

    the buttons work now, I need the music to loop properly durring the battle phase.

--- Let's Fight Button.gd
+++ Let's Fight Button.gd
@@ -9,3 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://BattleUI.tscn")
