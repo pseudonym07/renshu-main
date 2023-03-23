0e50001c0f4605e9392f7aad5a30acadb1bd1bb7
pseudonym07 <student.150267@worcesterschools.net>
2023-03-23 13:50:49 -0400

    quick upload

--- Entry Box.gd
+++ Entry Box.gd
@@ -1,6 +1,19 @@
 extends TextEdit
+var playerName = ""
 
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			get_tree().quit()
 
-
-func _on_text_set():
-	pass # Replace with function body.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (playerName != get_text()):
+		playerName = get_text()
+		print(playerName)
+		
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ENTER:
+			get_tree().change_scene_to_file("res://BattleUI.tscn")
+			print(playerName)
---
+++ MainMenu.gd
@@ -0,0 +1,6 @@
+extends Node
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			get_tree().quit()
