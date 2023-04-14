d49b8ce3997e18d1cddf8753c2bce11227232d09
pseudonym07 <student.150267@worcesterschools.net>
2023-04-14 13:42:53 -0400

    bruh

--- PlayerNamePrompt.gd
+++ PlayerNamePrompt.gd
@@ -2,10 +2,9 @@ extends Label
 
 
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_text("The Sketch dragon appears! Defeat it, Hero " + AskPname.playerName + "!")
+	set_text("The Sketch Dragon appears, deafeat it \n" + AskPname.playerName[0].to_upper() + AskPname.playerName.substr(1,-1) + "!")
 	
 func _input(event):
 	if event is InputEventKey and event.pressed:
