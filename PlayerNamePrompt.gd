0b48afa80cd94e43e4bcb6b6e891010f8774259a
PAUKUNO <student.150267@worcesterschools.net>
2023-04-24 13:41:37 -0400

    Added Health Bar and Buttons
    Just need to start adding functions to everything then we'll have a legit FUNCTIONING game.

--- PlayerNamePrompt.gd
+++ PlayerNamePrompt.gd
@@ -4,7 +4,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_text("The Sketch Dragon appears, deafeat it \n" + AskPname.playerName[0].to_upper() + AskPname.playerName.substr(1,-1) + "!")
+	set_text("The Sketch Dragon appears, defeat it \n" + AskPname.playerName[0].to_upper() + AskPname.playerName.substr(1,-1) + "!")
 	
 func _input(event):
 	if event is InputEventKey and event.pressed:
